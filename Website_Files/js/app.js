4dc2e91febd40423c4a49cda646eb2a9ec9093d0
thre3firstnames <natkmerry@gmail.com>
2022-05-28 19:41:03 -0600

    All javascript functional

--- Website_Files/js/app.js
+++ Website_Files/js/app.js
@@ -15,8 +15,8 @@ function init() {
   
       // Use the first sample from the list to build the initial plots
       var firstFacility = facNames[0];
-    //   buildCharts(firstSample);
       buildMetadata(firstFacility);
+      buildViolations(firstFacility);
     });
   }
   
@@ -24,9 +24,9 @@ function init() {
   init();
   
   function optionChanged(newFacility) {
-    // Fetch new data each time a new sample is selected
+    // Fetch new data each time a new facility is selected
     buildMetadata(newFacility);
-    // buildCharts(newSample);
+    buildViolations(newFacility);
     
   }
   
@@ -42,13 +42,6 @@ function init() {
   
       // Use `.html("") to clear any existing metadata
       PANEL.html("");
-  
-      // Use `Object.entries` to add each key and value pair to the panel
-      // Hint: Inside the loop, you will need to use d3 to append new
-      // tags for each key-value in the metadata.
-      // Object.entries(result).forEach(([key, value]) => {
-      //   PANEL.append("h6").text(`${key.toUpperCase()}: ${value.to}`);
-      // });
 
       PANEL.append("h3").text(result.facility_name);
       PANEL.append("h4").append("em").text(" in " + result.city);
@@ -59,19 +52,81 @@ function init() {
       PANEL.append("h6").text("Average Inspection Score: ").append("infodisplay").text(result.avg_inspection_score);
       PANEL.append("h6").text("Average Count of Violations: ").append("infodisplay").text(result.avg_violations_count);
   
-  
-    
+    }
+  )}
     // Use d3 to select the panel with id of `#fac-violation data`
-    var PANEL = d3.select("#fac-violation-data");
+  function buildViolations(facility) {
+    d3.json("web_facility_data.json").then((data) => {
+      var violations = data.violations;
+      // Filter the data for the object with the desired facility name
+      var resultArray = violations.filter(facObj => facObj.facility_name_plus == facility);
+      var result = resultArray[0];
+      console.log(result)
+      // Use d3 to select the panel with id of `#fac-metadata`
+      var PANEL = d3.select("#fac-violation-data");
   
-    // Use `.html("") to clear any existing metadata
-    PANEL.html("");
+      // Use `.html("") to clear any existing metadata
+      PANEL.html("");
 
-    PANEL.append("h3").text("Recieved Violations");
-    PANEL.append("h6").text("stuff goes here");
+      // PANEL.append("h3").text("Recieved Violations");
+      // PANEL.append("infodisplay").text(result.violations);
+      // Object.entries(result.violations).forEach((value) => {
+        let availableData = result.violations;
+        console.log(availableData)
+        replaceCommaLine(availableData);
+        function replaceCommaLine(data) {
+            //convert string to array and remove whitespace
+            let dataToArray = data.split('_').map(item => item.trim());
+            console.log(dataToArray)
+            // let manipData = dataToArray[dataToArray.length - 1]
+            // console.log(manipData)
+            // let lineBreak = "<br>"
+            //convert array to string replacing comma with new line
+            // return dataToArray.join(`${lineBreak}`)
+            return Object.values(dataToArray).forEach(val => {PANEL.append("violationsdis").text(`${val}`)})
+        }
+
+        
+        // PANEL.append("infodisplay").text(`${desiredData}`);
+      });
+    // });
+      // PANEL.append("h3").text(result.facility_name);
+      // PANEL.append("h4").append("em").text(" in " + result.city);
+      // PANEL.append("h6").text("Facility ID: ").append("infodisplay").text(result.facility_id);
+      // // PANEL.append("small").text(result.facility_id);
+      // PANEL.append("h6").text("Google Rating: ").append("infodisplay").text(result.rating);
+      // PANEL.append("h6").text("Total Google Ratings: ").append("infodisplay").text(result.total_ratings)
+      // PANEL.append("h6").text("Average Inspection Score: ").append("infodisplay").text(result.avg_inspection_score);
+      // PANEL.append("h6").text("Average Count of Violations: ").append("infodisplay").text(result.avg_violations_count);
+
+    
+    // var PANEL = d3.select("#fac-violation-data");
+  
+    // // Use `.html("") to clear any existing metadata
+    // PANEL.html("");
+
+    // PANEL.append("h3").text("Recieved Violations");
+    // PANEL.append("infodisplay").text(result.violations[""]);
+    //  console.log(result.violations[""]);
+    // // let availableData = result.violations;
+    // // let desiredData = replaceCommaLine(availableData);
+    // // function replaceCommaLine(data) {
+    // //     //convert string to array and remove whitespace
+    // //     let dataToArray = data.split(',').map(item => item.trim());
+      
+    // //     //convert array to string replacing comma with new line
+    // //     return dataToArray.join("\n");
+
+    // // }
+    // // Object.values(result.violations).forEach(([key, value]) => {
+    // //   PANEL.append("h6").text(`${key.append("br")} ${value}`);
+
+    // // });
+    // // console.log(result.violations[0]);
 
 
-  });
+    // // PANEL.append("h6").append("infodisplay").text(desiredData);
+
 
   }
 
